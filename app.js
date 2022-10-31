a887217d8f9e85613636494d438e829de6f73409
Ilya Navumenka <navumenkawork@gmail.com>
2022-10-31 04:32:24 -0700

    refactor: remove excess classes for tegs li in HTML, rule 3.3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
-    listItem.className = 'task-item task-item_incompleted';
+    listItem.className = 'task-item';
     label.innerText=taskString;
     label.className='task-name';
 
